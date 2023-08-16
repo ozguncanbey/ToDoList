bf46bb1327b9849fb9168dfe2c611a18ba84e9a5
Özgün Can Beydili <ozguncanbeydili@hotmail.com>
2023-08-16 13:53:30 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Özgün Can Beydili on 16.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
